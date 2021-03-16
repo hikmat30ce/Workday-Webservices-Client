2db642584dd03eb9872779053dca924a4220baee
Hikmat <me@hikmatu.com>
2021-03-16 19:35:00 +0500

    test cases added

--- test_wwsclient.py
+++
@@ -1,2 +0,0 @@
-print("success")
-assert 1 == 1
---
+++ tests/test_points.py
@@ -0,0 +1,11 @@
+def test_attributes_return_correctly():
+    p1 = 1
+    p2 = 2
+    assert (p1 == 1)
+    assert (p2 == 2)
+
+
+def test_string():
+    p1 = "wwsclient"
+    p2 = "workday"
+    assert p1 is not p2
